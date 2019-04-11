1022d7f6fc20749234cdefda5ed6295c8e6fdef4
pavelp21 <pavelvp2182@yandex.ru>
2019-04-11 17:27:50 +0500

    Main page is adepted

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -3,6 +3,18 @@
 //@prepros-prepend jquery.fancybox.min.js
 
 $(document).ready(function() {
+    $(function () {
+        var isIE = false || !!document.documentMode;
+
+        if (isIE) {
+            var head  = document.getElementsByTagName("head")[0];
+            var link  = document.createElement("link");
+            link.rel  = "stylesheet";
+            link.href = "css/ie.css";
+            head.appendChild(link);
+        }
+    });
+
   /* Работа формы */
   $(function () {
     var check = $('.check', this),
